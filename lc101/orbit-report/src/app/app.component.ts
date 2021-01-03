7ac9604b8d7973a8fdebe922b0a4e99e52c9c1e0
Amber Baumann <ambermindsolliving@gmail.com>
2021-01-03 17:50:03 -0600

    defined sourcelist array

---
+++ lc101/orbit-report/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'orbit-report';
+}
+const sourceList: Satellite[];
