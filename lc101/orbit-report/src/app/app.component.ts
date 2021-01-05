c02a5d714635db13f388e153c1752f7695dae5fc
Amber Baumann <ambermindsolliving@gmail.com>
2021-01-04 20:02:16 -0600

    finished part 6

--- lc101/orbit-report/src/app/app.component.ts
+++ lc101/orbit-report/src/app/app.component.ts
@@ -21,13 +21,12 @@ export class AppComponent {
 
         let fetchedSatellites = data.satellites;
         // TODO: loop over satellites
+        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
         for (let i = 0; i < fetchedSatellites.length; i++) {
           let newSat = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(newSat)
         }
-        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-
       }.bind(this));
     }.bind(this));
 
